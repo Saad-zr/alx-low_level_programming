ca1402a2a1fdd737d64fe6c9a6cedd7830f9c5b9
Saad-zr <saadzrioul0@gmail.com>
2023-03-23 19:46:55 -0700

    101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,10 +1,10 @@
 #include "main.h"
-
 /**
  * print_number - prints an integer
- * @n: input integer parameter
-**/
-
+ * @n:.input integer parameter
+ *
+ *Owned by Bwave ICT/ Bright Daniel
+ */
 void print_number(int n)
 {
 	unsigned int i = n;
